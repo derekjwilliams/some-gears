8cdcaa963a0ddc4acc1357edbd8f619184dd0273
Derek Williams <derek61@gmail.com>
2020-03-29 11:05:04 -0600

    css grid for collection group in Mimi example

--- ui/web/react/mimi/one/src/index.js
+++ ui/web/react/mimi/one/src/index.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+const pageTitleStyle = {
+  fontFamily: 'Lucida Grande',
+  fontSize: '22px'
+}
+
+const collectionGroupStyle = {
+  display: 'grid',
+  backgroundColor: '#ababab',
+  gridGap: '0.75rem',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(240px, 1fr))',
+  padding: '0.75rem',
+  boxSizing: 'border-box'
+}
+
+const collectionStyle = {
+  fontFamily: 'Lucida Grande',
+  fontSize: '14px',
+  backgroundColor: '#f6f3ea',
+  boxShadow: '1px 2px 8px rgba(0, 0, 0, 0.4)',
+  padding: '0.2rem'
+}
+
+const imageContainerStyle = {
+  marginTop: '1rem',
+  display: 'inline-block',
+  clear: 'both'
+}
+
+const imageStyle = {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover'
+}
+
+const titleStyle = {
+  fontStyle: 'italic',
+  fontSize: '1.2rem',
+  marginTop: '1.2rem'
+}
+const footerStyle = {
+  fontStyle: 'italic',
+  fontSize: '0.8rem',
+  marginTop: '0.8rem'
+}
+
+function Collections() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      allCollections {
+        nodes {
+          id
+          title
+          href
+          thumbnailImageUrl
+        }
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.log(JSON.stringify(error))
+    return <p>Error :(</p>
+  }
+  return <div style={collectionGroupStyle}>{data.allCollections.nodes.map((
+    { 
+      id,
+      title, 
+      href,
+      thumbnailImageUrl}) => (
+        <div key={id} style={collectionStyle}>
+          <div style = {titleStyle}>
+            <a href={href}>{title}</a>
+          </div>
+          <div style = {imageContainerStyle}>
+            <img style = {imageStyle} src={thumbnailImageUrl} alt={title + ' thumbnal image'}/>
+          </div>
+          <div style={footerStyle}>
+            <p>id: {id}</p>
+          </div>
+        </div>
+  ))}</div>
+}
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+    <h2 style={pageTitleStyle}>Collections</h2>
+      <Collections />
+    </div>
+  </ApolloProvider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));
+
